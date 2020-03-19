093d3fb6cb56ec799d2577c603576d397feae85d
Tony Larcombe <larcombe@gmail.com>
2020-03-19 12:22:25 +0200

    Update extract-email.sh

--- extract-email.sh
+++ extract-email.sh
@@ -2,3 +2,6 @@ grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" {filename.txt}
 
 
 grep -o '[[:alnum:]+\.\_\-]*@[[:alnum:]+\.\_\-]*' {filename.txt}
+
+
+grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b" filename.txt
