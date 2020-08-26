0e46d408436f5f4fa73dc6a93f73dcb6987763a1
Tony Larcombe <larcombe@gmail.com>
2020-08-26 16:27:09 +0300

    Create geolocate.sh

---
+++ geolocate.sh
@@ -0,0 +1,8 @@
+#This is using the free api key - I would rather you didn't rip it off but it is trivial to recreate.
+#Signup and the api key are free - just go to https://ipdata.co to get yours.
+
+cat inputips.csv | while read IPS; do 
+
+curl https://api.ipdata.co/"$IPS"?api-key=1f25ff40e5e1bdde99983aa901bc64cf20e15fbead346c1d5ee469cb >> output.txt;
+
+done
