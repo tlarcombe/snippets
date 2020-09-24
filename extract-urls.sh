c748537fdbd124c03c6952fbda37300f065a00ca
Tony Larcombe <larcombe@gmail.com>
2020-09-24 23:48:02 +0300

    Update extract-urls.sh

--- extract-urls.sh
+++ extract-urls.sh
@@ -1,14 +1,8 @@
 #Get truncated urls
 
-wget -qO- http://google.com/ |
-grep -Eoi '<a [^>]+>' |
-grep -Eo 'href="[^\"]+"' |
-grep -Eo '(http|https)://[^/"]+'
+wget -qO- http://google.com/ | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '(http|https)://[^/"]+'
 
 
 #Get urls with sibdirectories
 
-cat {sourcefile} |
-grep -Eoi '<a [^>]+>' |
-grep -Eo 'href="[^\"]+"' |
-grep -Eo '(http|https)://[^?"]+' 
+cat {sourcefile} | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '(http|https)://[^?"]+' 
