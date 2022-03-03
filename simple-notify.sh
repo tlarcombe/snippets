880d28d5c932d69da351ec596346de98d8b8ca58
Tony Larcombe <larcombe@gmail.com>
2022-03-03 08:48:10 +0200

    Create simple-notify.sh

---
+++ simple-notify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sleep 10
+notify-send "notify.sh" "Task #1 was completed successfully"
+
+
+
+#MAC VERSION
+#!/bin/bash
+sleep 10
+osascript -e "display notification \"Task #1 was completed successfully\" with title \"notify.sh\""
