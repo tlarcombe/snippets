20828ff9eb69662a87c20bcef74dc7fe76e20d93
Tony Larcombe <larcombe@gmail.com>
2022-03-03 08:47:05 +0200

    Create simple-anim.sh

---
+++ simple-anim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+while true;
+do
+    # Frame #1
+    printf "\r< Loading..." 
+    sleep 0.5
+    # Frame #2 
+    printf "\r> Loading..." 
+    sleep 0.5 
+done
+
+
+
+
+#!/bin/bash
+sleep 5 &
+pid=$!
+frames="/ | \\ -"
+while kill -0 $pid 2&>1 > /dev/null;
+do
+    for frame in $frames;
+    do
+        printf "\r$frame Loading..." 
+        sleep 0.5
+    done
+done
+printf "\n"
